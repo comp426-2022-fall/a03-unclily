2d4739a4ff0036c45f46beab26cf1aac6a5c25cf
Lisa Lee <unclily@live.unc.edu>
2022-11-29 23:02:42 -0500

    a03

---
+++ bin/cli.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import { roll } from "/lib/roll.js"
+import minimist from "minimist";
+
+const args = minimist(process.argv.slice(2));
+
+const sides = args.sides;
+const dice = args.dice;
+const rolls = args.rolls;
+const results = roll(sides, dice, rolls);
+
+console.log(results);
